8c54389ab9acad104334046b59e681a3253c722b
mollygardner <44197982+mollygardner@users.noreply.github.com>
2018-10-17 22:08:24 +0100

    Update Encryption.java

--- src/com/company/Encryption.java
+++ src/com/company/Encryption.java
@@ -1,6 +1,48 @@
 package com.company;
 
 public class Encryption {
+    ShowMenu(); 
+    int choice = GetChoice();  
+    if (choice == 1 ) 
+    {  
+      Caesar caes = new Caesar(); 
+        caes.
+       
+    } 
+    if (choice == 2) 
+    { 
+      Vernam vern = new Vernam(); 
+       
+    } 
+    if (choice == 3) 
+    { 
+      Veignere veig = new Veignere(); 
+      veig.
+    
+    } 
+ 
+   ShowMenu()
+  private static void () 
+  {  
+    System.out.println("Enter"); 
+    System.out.println("        OR"); 
+    System.out.println("Enter "); 
+    System.out.println("        OR"); 
+    System.out.println("Enter "); 
+    System.out.println("        OR"); 
+    System.out.println("Enter "); 
+ 
+  } 
+ 
+  private static int GetChoice() 
+  { 
+    Scanner s = new Scanner(System.in); 
+ 
+    while (true) 
+    { 
+      String data = s.nextLine(); 
+      if (data.equals("")) 
+      { 
     private String plaintext;
     private String cyphertext;
     private String key;
